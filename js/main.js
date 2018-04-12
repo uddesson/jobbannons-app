a4350ffcde0c8e5388aa1c250557d8c5e9a11067
Emma Uddesson <emma.uddesson@gmail.com>
2018-04-12 12:17:54 +0200

    Create FetchModel, making function calls easier

--- js/main.js
+++ js/main.js
@@ -13,17 +13,25 @@ class Fetch {
 }
 
 const jobs = new Fetch('http://api.arbetsformedlingen.se/af/v0/platsannonser/');
-jobs.fetchAll()
-.then(jobs => {
-    View.displayJobs(jobs);
-    View.displayNumberOfJobs(jobs);
-});
 
-
-jobs.fetchOne('7665589')
-.then(job => {
-    View.displayOneJob(job);
-});
+const FetchModel = (function(){
+    return{
+        fetchAllJobs: function(){
+            jobs.fetchAll()
+            .then(jobs => {
+                View.displayJobs(jobs);
+                View.displayNumberOfJobs(jobs);
+            });
+        },
+        
+        fetchSingleJob: function(id){
+            jobs.fetchOne(id)
+            .then(job => {
+                View.displayOneJob(job);
+            });
+        }
+    }
+}());
 
 
 const View = (function(){
@@ -37,7 +45,7 @@ const View = (function(){
             let jobInfo = ``;
 
             for(let job of jobs){
-                jobInfo += `<div id="${job.annonsid}" class="job-wrapper">
+                jobInfo += `<div class="job-wrapper">
                 <h2>${job.annonsrubrik}</h2>
                 <p>Arbetsplats: ${job.arbetsplatsnamn}</p>
                 <p>Plats: ${job.kommunnamn}</p>
@@ -45,8 +53,8 @@ const View = (function(){
                 <p>Yrkesbenämning: ${job.yrkesbenamning}</p>
                 <p>Anställningstyp: ${job.anstallningstyp}</p>
                 <p>${job.annonsurl}</p>
-                <button id="saveAd">Spara annons</button>
-                <button id="showAd">Visa annons</button>
+                <button class="saveAd">Spara annons</button>
+                <button class="showAd" data-id="${job.annonsid}">Visa annons</button>
                 </div>`;
             }
             wrapper.innerHTML = jobInfo;
@@ -61,18 +69,23 @@ const View = (function(){
                jobbannonser i Stockholms län</h2>`;
                numberOfJobsWrapper.innerHTML = jobInfo;
         },
-        
+
         displayOneJob: function(job){
             job = job.platsannons;
 
             let jobInfo = ``;
                 jobInfo += `<div id="${job.annons.annonsid}" class="single-job-wrapper">
+                <button id="back">Tillbaka</button>
                 <h2>${job.annons.annonsrubrik}</h2>
                 <h3>Kommun: ${job.annons.kommunnamn}</h3>
                 <p>${job.annons.annonstext.replace(/(\r\n|\n|\r)/gm, '<br />')}</p>
                 </div>`;
 
-             wrapper.innerHTML = jobInfo;
+            wrapper.innerHTML = jobInfo;
+            var backButton = document.getElementById('back');
+            backButton.addEventListener('click', FetchModel.fetchAllJobs);
         }
      }
 }());
+
+FetchModel.fetchSingleJob('7665589');
