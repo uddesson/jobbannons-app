41cbec92033e54db1f9e515752011b0c16ce70b4
Emma Uddesson <emma.uddesson@gmail.com>
2018-04-12 11:08:21 +0200

    Add more details to job-output

--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
-
-
-
 class Fetch {
     constructor(baseUrl){
         this.baseUrl = baseUrl;
@@ -31,24 +28,26 @@ const View = (function(){
             jobs = jobs.matchningslista.matchningdata;
             let jobInfo = ``;
             for(let job of jobs){
-                
-                jobInfo += `<div class="job-wrapper">
+                jobInfo += `<div id="${job.annonsid}" class="job-wrapper">
                 <h2>${job.annonsrubrik}</h2>
-                <p>${job.arbetsplatsnamn}</p>
-                <p>${job.kommunnamn}</p>
+                <p>Arbetsplats: ${job.arbetsplatsnamn}</p>
+                <p>Plats: ${job.kommunnamn}</p>
                 <p>Sista ansökningsdag: ${job.sista_ansokningsdag}</p>
                 <p>Yrkesbenämning: ${job.yrkesbenamning}</p>
                 <p>Anställningstyp: ${job.anstallningstyp}</p>
                 <p>${job.annonsurl}</p>
+                <button id="saveAd">Spara annons</button>
                 </div>`;
             }
             wrapper.innerHTML = jobInfo;
         },
 
+        // Shows total number of jobs in Stockholm county
         displayNumberOfJobs: function(jobs) {
             totalJobs = jobs.matchningslista.antal_platsannonser;
             let jobInfo = ``;
-               jobInfo = `<h2>${totalJobs}</h2>`;
+               jobInfo = `<h2>Just nu finns ${totalJobs} 
+               jobbannonser i Stockholms län</h2>`;
                numberOfJobsWrapper.innerHTML = jobInfo;
         }
      }
